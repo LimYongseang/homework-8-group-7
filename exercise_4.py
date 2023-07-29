6bd3cf7fa3714ac8a3f7ce065526abfef7bb4c82
LimYongseang <2023171lim@aupp.edu.kh>
2023-07-30 01:11:08 +0700

    test and def 4 and 5 and testall

--- exercise_4.py
+++ exercise_4.py
@@ -2,9 +2,9 @@ def chunking_by(numbers, chunck):
     result = []
     temp = []
     time = 0
-    if not numbers:
+    if not numbers or type(chunck) != int:
         return result  
-    elif chunck == 0 or type(chunck) != int:
+    elif chunck == 0:
         return numbers
     else:
         for number in numbers:
@@ -20,3 +20,6 @@ def chunking_by(numbers, chunck):
     return result
     
 
+# print(chunking_by([5, 4, 7, 3, 4, 5, 4], 3))
+# chunking_by([5, 4, 7, 3, 4, 5, 4], 3) == [[5, 4, 7], [3, 4, 5], [4]]
+# chunking_by([3, 4, 5], 1) == [[3], [4], [5]]
