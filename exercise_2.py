821c15050d49f856669070db65d8fb431d27e15d
FongBunsong <137795662+FongBunsong@users.noreply.github.com>
2023-07-30 10:37:58 +0700

    Update exercise_2.py

--- exercise_2.py
+++ exercise_2.py
@@ -1,8 +1,8 @@
-def index_power(numbers, n):
-    if n < 0 or n >= len(numbers) :
+def index_power(numnbers, n):
+    if n < 0 or n >= len(numnbers): 
         return -1
     else : 
-        return numbers[n] ** n
+        return numnbers[n] ** n
     
 print(index_power([1, 2, 3, 4], 2))
 print(index_power([1, 3, 10, 100], 3)) 
