dbfd3dfee7da83289c029e9817925c4d496906a6
LimYongseang <2023171lim@aupp.edu.kh>
2023-07-30 22:22:30 +0700

    Update test_4.py

--- test_4.py
+++ test_4.py
@@ -14,7 +14,7 @@ def test_chunking_by():
     assert chunking_by([1, 3, 4, 5, 1, 2, 1], 0) == [1, 3, 4, 5, 1, 2, 1]
 
 
-# test invalid
+# test invalid inputs
 def test_chunking_by():
     assert chunking_by([1, 2, 4, 6, 6], "a") == [1, 2, 4, 6, 6]
     assert chunking_by([9, 8, 5, 3, 2], "m") == [9, 8, 5, 3, 2]
