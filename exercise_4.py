7787bb98b210553afc155f0199238bbeab662649
Chhin19 <2023323chhin@aupp.edu.kh>
2023-08-01 16:06:23 +0700

    Update exercise_4.py

--- exercise_4.py
+++ exercise_4.py
@@ -1,19 +1,25 @@
 def chunking_by(numbers, chunck):
-    result = []
-    temp = []
-    time = 0
-    if not numbers:
-        return result
-    elif chunck == 0 or type(chunck) != int:
-        return numbers
-    else:
-        for number in numbers:
-            temp.append(number)
-            time += 1
+   
+        result = []
+        temp = []
+        time = 0
+        if not numbers:
+            return result
+        elif chunck == 0 or type(chunck) != int:
+            return numbers
+        else:
+            for number in numbers:
+                temp.append(number)
+                time += 1
 
-            if time == chunck:
-                result += [temp]
-                temp = []
-                time = 0
-        result += [temp]
-    return result
+                if time == chunck:
+                    result += [temp]
+                    temp = []
+                    time = 0
+            result += [temp]
+        return result
+
+# numbers = [1,2,3,4,5,6,7,8,9,10,11]
+# chunck = 4
+# print(chunking_by(numbers, chunck))
+
