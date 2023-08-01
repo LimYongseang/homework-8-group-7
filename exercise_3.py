0327c3d604142214222ca4cacab32c836fa8cf7b
NgarSopheaktra <137017933+NgarSopheaktra@users.noreply.github.com>
2023-08-01 08:41:35 +0700

    Update exercise_3.py

--- exercise_3.py
+++ exercise_3.py
@@ -6,10 +6,11 @@ def remove_all_after(numbers, n):
     
     if n in numbers:
         return numbers[0:numbers.index(n)+1]
-
-a = remove_all_after([1, 2, 3, 4, 5], 4)
-print(a)
-b = remove_all_after([1, 1, 2, 2, 3, 3], 4)
-print (b)
-c = remove_all_after([1, 1, 2, 2, 3, 3], 2)
-print (c)
+    else:
+        return numbers
+# a = remove_all_after([1, 2, 3, 4, 5], 4)
+# print(a)
+# b = remove_all_after([1, 1, 2, 2, 3, 3], 4)
+# print (b)
+# c = remove_all_after([1, 1, 2, 2, 3, 3], 2)
+# print (c)
