9a9168358aa892b0d5711e8ec9b8db8dff446fa8
NgarSopheaktra <137017933+NgarSopheaktra@users.noreply.github.com>
2023-08-01 13:16:09 +0700

    Update exercise_3.py

--- exercise_3.py
+++ exercise_3.py
@@ -8,9 +8,32 @@ def remove_all_after(numbers, n):
         return numbers[0:numbers.index(n)+1]
     else:
         return numbers
-# a = remove_all_after([1, 2, 3, 4, 5], 4)
-# print(a)
-# b = remove_all_after([1, 1, 2, 2, 3, 3], 4)
-# print (b)
-# c = remove_all_after([1, 1, 2, 2, 3, 3], 2)
-# print (c)
+    
+a = remove_all_after([1, 2, 3, 4, 5], 3)
+print(a)
+b = remove_all_after([1, 1, 2, 2, 3, 3], 4)
+print (b)
+c = remove_all_after([1, 1, 2, 2, 3, 3], 2)
+print (c)
+d = remove_all_after([1, 2, 3, 4, 5], 3)
+print(d)
+e = remove_all_after([1, 1, 2, 2, 3, 3], 2)
+print (e)
+f = remove_all_after([], 0)
+print (f)
+g = remove_all_after([], 3)
+print (g)
+h = remove_all_after([1, 2, 3, 4, 5], 6)
+print (h)
+i =remove_all_after(
+        [
+            0,
+            4,
+            5,
+            6,
+            6,
+        ],
+        1,
+    )
+print (h)
+print(i)
