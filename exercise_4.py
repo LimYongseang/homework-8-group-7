20a57051d25cb992761da4d79c8f8fa8907a2cc8
Chhin19 <2023323chhin@aupp.edu.kh>
2023-08-01 10:17:41 +0700

    e.4

--- exercise_4.py
+++ exercise_4.py
@@ -1,28 +1,19 @@
 def chunking_by(numbers, chunck):
-    for i in range(0, len(numbers), chunck):
-        yield numbers[i:i + chunck]
+    result = []
+    temp = []
+    time = 0
+    if not numbers:
+        return result
+    elif chunck == 0 or type(chunck) != int:
+        return numbers
+    else:
+        for number in numbers:
+            temp.append(number)
+            time += 1
 
-numbers = [1,2,3,4,5,6,7,8,9,10]   
-chunck = 2
-print(list(chunking_by(numbers, chunck)))  
-
-
-
-    # result = []
-    # temp = []
-    # time = 0
-    # if not numbers:
-    #     return result
-    # elif chunck == 0 or type(chunck) != int:
-    #     return numbers
-    # else:
-    #     for number in numbers:
-    #         temp.append(number)
-    #         time += 1
-
-    #         if time == chunck:
-    #             result += [temp]
-    #             temp = []
-    #             time = 0
-    #     result += [temp]
-    # return result
+            if time == chunck:
+                result += [temp]
+                temp = []
+                time = 0
+        result += [temp]
+    return result
